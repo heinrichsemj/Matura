cf6e315981b23ebe8a9a457f15f3fa1c2c2bff46
heinrichsemj <85365063+heinrichsemj@users.noreply.github.com>
2023-01-04 13:13:20 +0100

    Update OzonDaten.py

--- OzonDaten.py
+++ OzonDaten.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 
+##Variabeln und Listen
 t_log = 0
 c = []
 t = []
@@ -9,9 +10,13 @@ count = True
 counter = 0
 platzhalter_t = 0
 vari = 999
+t_labels = []
+t_labelsnum = []
+daysit = 0
+dayline = []
 
 
-def integer_to_time_stamp(time_as_integer):
+def integer_to_time_stamp(time_as_integer): #eine Zahl zu Time stamp
     hours = time_as_integer // 3600
     remaining_seconds = time_as_integer % 3600
     minutes = remaining_seconds // 60
@@ -20,7 +25,7 @@ def integer_to_time_stamp(time_as_integer):
     return time_stamp
 
 
-def time_as_integer(time_stamp):
+def time_as_integer(time_stamp): #time stamp zu einer Zahl
     time_stamp_parts = time_stamp.split(":")
     hours = int(time_stamp_parts[0])
     minutes = int(time_stamp_parts[1])
@@ -30,7 +35,7 @@ def time_as_integer(time_stamp):
     return hours * 3600 + minutes * 60 + seconds
 
 
-while count == True:
+while count == True: #Daten aus 'OzonDaten.txt' lesen
     line = f.readline()
     if line[0:4] == "Stop":
         count = False
@@ -45,11 +50,9 @@ while count == True:
     t_numvalue.append(time_as_integer(t[counter]))
     c.append(float(line[35:40]))
     counter += 1
-print(t_log)
-t_labels = []
-t_labelsnum = []
-daysit = 0
-for i in range(20):
+   
+
+for i in range(20): #Beschriftung der X-Achse (Zeit) verändern
     if daysit == 1:
         t_labels.append(integer_to_time_stamp(12 * 60 * 60)[0:5])
         daysit = 0
@@ -57,15 +60,16 @@ for i in range(20):
         t_labels.append(integer_to_time_stamp(24 * 60 * 60)[0:5])
         daysit = 1
     t_labelsnum.append(12 * 60 * 60 * i)
+    
+    
 f.close()
 plt.xlabel('Zeit')
 plt.ylabel('Ozon Konzentration in parts per billion (ppb)')
 plt.title('Ozon Konzentration zwischen 19.11.2022 und 28.11.2022')
 plt.plot(t_numvalue, c, marker=',', color='b')
-
 plt.xticks(t_labelsnum, t_labels)
-dayline = []
-for i in range(10):
+
+for i in range(10): #Vertikale Linien und Datum einfügen
     plt.text((24 * 60 * 60 * i)+1000, 60, str(19 + i) + ".11.2022")
     dayline.append(24 * 60 * 60 * i)
     plt.axvline(x=dayline[i], color='r')

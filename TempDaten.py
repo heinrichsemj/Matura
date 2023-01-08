e75de5bb93ad31fd240ef418e9efbf2a3e32f405
Enrique Baltis <85365063+heinrichsemj@users.noreply.github.com>
2023-01-08 12:55:05 +0100

    Update TempDaten.py

--- TempDaten.py
+++ TempDaten.py
@@ -6,19 +6,17 @@ temp = []
 temp_time = []
 
 
-for i in range(11):
+for i in range(10):
     line = float(Temp.readline())
     temp.append(line)
 Temp.close()
-for i in range(11):
-    if i != 10:
-        plt.text((24 * 60 * 60 * i)+1000, 5.4, str(19 + i) + ".11.2022")
+for i in range(10):
+    plt.text((24 * 60 * 60 * i)+1000, 5.4, str(19 + i) + ".11.2022")
     temp_time.append((24 * 60 * 60 * i))
     plt.axvline(x=temp_time[i], color='g')
-    print(temp[i])
     plt.hlines(y=temp[i], xmin=(24 * 60 * 60 * i), xmax=(24 * 60 * 60 * (i+1)), color='r')
 plt.xlabel('Zeit')
 plt.ylabel('Temperatur in °C')
 plt.title('Durchschnittliche Temperatur zwischen 19.11.2022 und 28.11.2022')
-
+plt.xlim(left=0, right=24 * 60 * 60 * 10)
 plt.show()
